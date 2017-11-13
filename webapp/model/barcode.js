c31b741077c73ab86543539e5c3f9810ebd1b1b7
HONGU Dorin <dhongu@gmail.com>
2017-11-13 10:25:53 +0000

    Barcode

--- webapp/model/barcode.js
+++ webapp/model/barcode.js
@@ -3,47 +3,48 @@ sap.ui.define(function() {
 
 	var BarCodeScanner = {
 
-		connect: function(callback) {
+		handler: function(event) {
 			var code = "";
 			var timeStamp = 0;
 			var timeout = null;
 
-			this.handler = function(event) {
-				if (timeStamp + 50 < new Date().getTime()) {
-					code = "";
-				}
- 
+			if (timeStamp + 50 < new Date().getTime()) {
+				code = "";
+			}
 
-				timeStamp = new Date().getTime();
-				clearTimeout(timeout);
+			timeStamp = new Date().getTime();
+			clearTimeout(timeout);
 
-				if (event.which != 13) { // ignore returns
-					code += String.fromCharCode(event.which);
-				}
-				timeout = setTimeout(function() {
-					var focus = $(":focus");
-				    if (focus == undefined || focus.length === 0) {
-						if (code.length >= 3) {
-							callback(code);
-						}
-				    }
-					else {
-						if (code.length >= 3 &&  focus[0].tagName != "INPUT") {
-							callback(code);
-						}
-						else{
-							// este intr-un camp de input si trebuie sa accept introducerea prin scanare
-						 
-						}
+			if (event.which != 13) { // ignore returns
+				code += String.fromCharCode(event.which);
+			}
+			timeout = setTimeout(function() {
+				var focus = $(":focus");
+				if (focus == undefined || focus.length === 0) {
+					if (code.length >= 3) {
+						this.callback(code);
 					}
-				    
-					code = "";
-				}, 100);
-			};
-
-			$('body').on('keypress', this.handler);
-
+				} else {
+					if (code.length >= 3) {
+						if (focus[0].tagName != "INPUT") {
+							event.target.value = event.target.value.replace(code, '');
+						}
+						this.callback(code);
+					}
+				}
+				code = "";
+			}, 100);
 		},
+
+		connect: function(callback) {
+			this.callback = callback;
+			$('body').on('keypress', this.handler);
+		},
+
+		element_is_editable: function(element) {
+			return $(element).is('input,textarea,[contenteditable="true"]');
+		},
+
 		disconnect: function() {
 			$('body').off('keypress', this.handler);
 		}
